da4156ae449118351a2cea1b6943b3d79d512439
Lohran Mendes <lohranfm@gmail.com>
2025-09-27 12:14:58 -0300

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace lohran_mendes_DR4_AT_S2.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
