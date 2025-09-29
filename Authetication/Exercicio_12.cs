b5be50a1c699c66f925ccc43be5b84f5e8ae3ca2
Lohran Mendes <lohranfm@gmail.com>
2025-09-29 11:47:55 -0300

    feat: add Exercicio_12 class for authentication implementation

---
+++ Authetication/Exercicio_12.cs
@@ -0,0 +1,7 @@
+﻿namespace lohran_mendes_DR4_AT_S2.Authetication;
+
+public class Exercicio_12
+{
+    // Não aprendemos em aula a fazer este exercício depois do projeto criado, apenas em um projeto novo.
+    // Então tentei reproduzir como foi pedido mas não consegui já que a questão pedia a implementação de autenticação no projeto já criado.
+}
