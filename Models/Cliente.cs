384a35542342062829b61d830f3fd94eccf228cf
Lohran Mendes <lohranfm@gmail.com>
2025-09-29 21:30:29 -0300

    feat: add validation attributes for Cliente and Destino models

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -5,7 +5,13 @@ namespace lohran_mendes_DR4_AT_S2.Models;
 public class Cliente
 {
     public int Id { get; set; }
+    
+    [Required(ErrorMessage = "O nome é obrigatório.")]
+    [MinLength(2, ErrorMessage = "O nome deve ter no mínimo 2 caracteres.")]
     public string Nome { get; set; }
+    
+    [Required(ErrorMessage = "O email é obrigatório.")]
+    [MinLength(2, ErrorMessage = "O email deve ter no mínimo 2 caracteres.")]
     public string Email { get; set; }
     public List<Reserva>? Reservas { get; set; }
 }
--- Models/Destino.cs
+++ Models/Destino.cs
@@ -5,7 +5,13 @@ namespace lohran_mendes_DR4_AT_S2.Models;
 public class Destino
 {
     public int Id { get; set; }
+    
+    [Required(ErrorMessage = "O nome é obrigatório.")]
+    [MinLength(2, ErrorMessage = "O nome deve ter no mínimo 2 caracteres.")]
     public string Nome { get; set; }
+    
+    [Required(ErrorMessage = "O país é obrigatório.")]
+    [MinLength(2, ErrorMessage = "O país deve ter no mínimo 2 caracteres.")]
     public string Pais { get; set; }
     public ICollection<DestinosPacotes>? DestinosPacotes { get; set; }
 }
